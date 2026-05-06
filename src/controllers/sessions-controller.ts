ef578c5b8d6ab726d5d054521e92e81f123c271e
Broninn <bruno-hs@outlook.com>
2026-05-06 16:09:45 -0300

    config user token

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,9 +1,43 @@
-import { Request, Response } from "express";
+import { AppError } from '@/utils/AppError';
+import { Request, Response } from 'express';
+import { prisma } from '@/database/prisma';
+import { compare } from 'bcrypt';
+import { z } from 'zod';
+import { authConfig } from '@/configs/auth';
+import { sign } from 'jsonwebtoken';
 
 class SessionsController {
-    create(req: Request, res:Response) {
-        return res.json({message: "created"})
+  async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { email, password } = bodySchema.parse(req.body);
+
+    const user = await prisma.users.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new AppError('Invalid email or password', 401);
     }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Invalid email or password', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ role: user.role ?? 'member' }, secret!, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return res.json(token);
+  }
 }
 
-export { SessionsController }
+export { SessionsController };
